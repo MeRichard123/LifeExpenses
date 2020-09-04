a76b6c7486dc94f9fbf329009e67b0416e2cb70e
lennyaiko <lennyaiko17@gmail.com>
2020-09-04 15:01:39 +0100

    todo for models has been configured

--- ExpenseTracker/todo/admin.py
+++ ExpenseTracker/todo/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Category, Todo
+
 
 # Register your models here.
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ['task', 'priority']
+    search_fields = ['category', 'task']
+    list_filter = ['category', 'priority']
+
+
+admin.site.register(Category)
+admin.site.register(Todo, TodoAdmin)
