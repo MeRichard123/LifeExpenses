f969eb768be0e77b715005ac399988c9ce488172
Richard <51269862+MeRichard123@users.noreply.github.com>
2020-09-05 11:36:42 +0100

    :zap: Home Page, User login, Register and basic dahsboard

---
+++ ExpenseTracker/accounts/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render,redirect
+from .forms import UserRegister
+from django.contrib import messages
+
+# Create your views here.
+
+
+def register(request):
+    if request.method == "POST":
+        Form = UserRegister(request.POST)
+        # UserCreation and validation comes from django
+        if Form.is_valid():
+            Form.save()  # save user and hash password
+            username = Form.cleaned_data.get("username")
+            messages.success(
+                request, f"Your account has been created. You can now log in")
+            return redirect("login")
+
+    else:
+        Form = UserRegister()
+    return render(request, "register.html", {'form': Form})
+
+
+def landing(req):
+    return render(req, "landing.html")
