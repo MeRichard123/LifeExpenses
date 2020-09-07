9ee0c3ae84b36caa148a491178bd54ef1581c8c7
Richard <51269862+MeRichard123@users.noreply.github.com>
2020-09-07 12:37:58 +0100

    Add models and fix bug

--- ExpenseTracker/accounts/views.py
+++ ExpenseTracker/accounts/views.py
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
-from django.shortcuts import render,redirect
-from .forms import UserRegister
-from django.contrib import messages
-=======
 from django.shortcuts import render, redirect
 from .forms import UserRegister
 from django.contrib import messages
 from .decorators import authenticated_user
 
->>>>>>> 73a66e842e2456b6653eefdd7112a3bfc3337f9d
 
 # Create your views here.
 
@@ -29,9 +23,6 @@ def register(request):
     return render(request, "register.html", {'form': Form})
 
 
-<<<<<<< HEAD
-=======
 @authenticated_user
->>>>>>> 73a66e842e2456b6653eefdd7112a3bfc3337f9d
 def landing(req):
     return render(req, "landing.html")
--- ExpenseTracker/todo/urls.py
+++ ExpenseTracker/todo/urls.py
@@ -3,14 +3,9 @@ from . import views
 
 
 urlpatterns = [
-<<<<<<< HEAD
     path("todo/", views.task, name="todo"),
     path("delete_task/<str:pk>", views.delete_task, name="delete_task"),
     path("create_task/", views.create_task, name="create_task"),
     path("done_task/<str:pk>", views.done_task, name="done_task"),
     path("update_task/<str:pk>", views.update_task, name="update_task"),
 ]
-=======
-    
-]
->>>>>>> 73a66e842e2456b6653eefdd7112a3bfc3337f9d
