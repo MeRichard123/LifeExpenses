ec345228bca00742b0b08cf3fc294dba6574b515
lennyaiko <lennyaiko17@gmail.com>
2020-09-13 11:44:48 +0100

    Added some finishing touches

--- ExpenseTracker/todo/forms.py
+++ ExpenseTracker/todo/forms.py
@@ -4,7 +4,6 @@ from .models import Todo, STATUS, PRIORITY
 
 
 class CreateTodo(ModelForm):
-    status = forms.ChoiceField(choices=STATUS, widget=forms.RadioSelect())
     priority = forms.ChoiceField(choices=PRIORITY, widget=forms.RadioSelect())
     task = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     description = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
@@ -12,5 +11,5 @@ class CreateTodo(ModelForm):
     class Meta:
         model = Todo
         fields = '__all__'
-        exclude = ['user']
+        exclude = ['user', 'status']
 
